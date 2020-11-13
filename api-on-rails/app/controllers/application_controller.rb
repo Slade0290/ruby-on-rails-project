bf102c86b11542be2020f7e41687d7ff15a2fe15
keffa98 <a.fonton@epmistes.net>
2020-11-13 14:41:02 +0100

    fix simple token auth

--- api-on-rails/app/controllers/application_controller.rb
+++ api-on-rails/app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
 
+    acts_as_token_authentication_handler_for User
+
+
     def home
         render 'layouts/home'
     end
--- api-on-rails/config/routes.rb
+++ api-on-rails/config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
+ 
 
   root 'application#home'
 
-  devise_for :users
+  # devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
   # root to: "cars#index"
 
   resources :cars
