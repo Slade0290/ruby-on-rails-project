d8b749ee7a7e0795bf040074ea4efb1c71381f16
Slade0290 <ly.david@live.fr>
2020-11-13 12:33:19 +0100

    refix conflict

--- api-on-rails/db/schema.rb
+++ api-on-rails/db/schema.rb
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 2020_11_12_114244) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "adminpack"
-  enable_extension "plpgsql"
-
-  create_table "cars", force: :cascade do |t|
-    t.string "brand"
-    t.string "model"
-    t.string "year"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-end
-=======
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -68,4 +39,3 @@ ActiveRecord::Schema.define(version: 2020_11_13_111128) do
   end
 
 end
->>>>>>> 02c76e8b09ed284f7a3d4e755dd98a9ca36520b7
