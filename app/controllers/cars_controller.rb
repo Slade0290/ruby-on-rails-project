a46d82e696c92b0a11e6a025d27039777de1a8ea
Slade0290 <ly.david@live.fr>
2020-11-29 22:49:03 +0100

    front in progress

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -3,17 +3,17 @@ class CarsController < ApplicationController
   require 'open-uri'
   require 'json'
 
-  def show
-    @cars = JSON.parse(open('https://bnyonrails.herokuapp.com/api/cars').string)
-    
-    # response = URI.open("https://bnyonrails.herokuapp.com/api/cars") {|f|
-    #   f.each_line {|line| p line}
-    # }
-    # puts "\n\n#{response}\n\n"
-    # puts "\n\n#{response[0]}\n\n"
-    # my_hash = JSON.parse(response[0])
-    # response.each {
+  $url = 'https://bnyonrails.herokuapp.com/api/cars/'
 
-    # }
+  def index
+    @cars = JSON.parse(open($url).string)
+  end
+
+  def show 
+    @car = JSON.parse(open($url + params[:id]).string)
+  end
+  
+  def edit 
+    @car = JSON.parse(open($url + params[:id]).string)
   end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,14 @@
 class UserController < ApplicationController
+  
+  $url = 'https://bnyonrails.herokuapp.com/api/user/'
+
   def create
+    # call api with data to create user in back
   end
+
+  def show 
+    # @user = JSON.parse(open($url + params[:id]).string)
+    
+  end
+
 end
